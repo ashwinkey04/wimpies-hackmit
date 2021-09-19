20fcb130b6d32f89bf34ef90f1f549ee10d00723
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2021-09-19 10:03:30 +0530

    FEAT: Introduce maps

--- lib/ui/screens/top_navigation_screens/event_screen.dart
+++ lib/ui/screens/top_navigation_screens/event_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tinder_app_flutter/data/db/remote/firebase_database_source.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class EventScreen extends StatefulWidget {
   const EventScreen({Key key}) : super(key: key);
@@ -11,7 +12,8 @@ class EventScreen extends StatefulWidget {
 class _EventScreenState extends State<EventScreen> {
   final FirebaseDatabaseSource _databaseSource = FirebaseDatabaseSource();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
-
+  GoogleMapController _controller;
+    
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +27,21 @@ class _EventScreenState extends State<EventScreen> {
                 'Events',
                 style: Theme.of(context).textTheme.headline3,
               ),
-            )
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(37.42796133580664, -122.085749655962),
+                  zoom: 14.4746,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller = controller;
+              },
+            ),
+            ),
           ],
         ));
   }
